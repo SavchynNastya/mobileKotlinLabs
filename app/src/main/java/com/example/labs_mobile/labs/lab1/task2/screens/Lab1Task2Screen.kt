35946f26c9ef611ef63573b96e7ebb68d65805cf
Anastasiia Savchyn <sa@empat.tech>
2024-09-15 22:17:14 +0300

    cleanup

--- app/src/main/java/com/example/labs_mobile/labs/lab1/task2/screens/Lab1Task2Screen.kt
+++ app/src/main/java/com/example/labs_mobile/labs/lab1/task2/screens/Lab1Task2Screen.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
